cb587acc5959088c406145009c862c9ef654709b
jamiefb <jamieb@mirageoscience.com>
2023-07-06 13:57:11 -0700

    move peak finder over from geoapps

---
+++ peak_finder/__init__.py
@@ -0,0 +1,11 @@
+#  Copyright (c) 2023 Mira Geoscience Ltd.
+#
+#  This file is part of peak-finder-app project.
+#
+#  All rights reserved.
+#
+
+from __future__ import annotations
+
+from .constants import app_initializer
+from .params import PeakFinderParams
---
+++ peak_finder/base/utils.py
@@ -0,0 +1,64 @@
+#  Copyright (c) 2023 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from __future__ import annotations
+
+import numpy as np
+
+
+def hex_to_rgb(hex_color):
+    """
+    Convert hex color code to RGB
+    """
+    code = hex_color.lstrip("#")
+    return [int(code[i : i + 2], 16) for i in (0, 2, 4)]
+
+
+def running_mean(
+    values: np.array, width: int = 1, method: str = "centered"
+) -> np.array:
+    """
+    Compute a running mean of an array over a defined width.
+
+    :param values: Input values to compute the running mean over
+    :param width: Number of neighboring values to be used
+    :param method: Choice between 'forward', 'backward' and ['centered'] averaging.
+
+    :return mean_values: Averaged array values of shape(values, )
+    """
+    # Averaging vector (1/N)
+    weights = np.r_[np.zeros(width + 1), np.ones_like(values)]
+    sum_weights = np.cumsum(weights)
+
+    mean = np.zeros_like(values)
+
+    # Forward averaging
+    if method in ["centered", "forward"]:
+        padded = np.r_[np.zeros(width + 1), values]
+        cumsum = np.cumsum(padded)
+        mean += (cumsum[(width + 1) :] - cumsum[: (-width - 1)]) / (
+            sum_weights[(width + 1) :] - sum_weights[: (-width - 1)]
+        )
+
+    # Backward averaging
+    if method in ["centered", "backward"]:
+        padded = np.r_[np.zeros(width + 1), values[::-1]]
+        cumsum = np.cumsum(padded)
+        mean += (
+            (cumsum[(width + 1) :] - cumsum[: (-width - 1)])
+            / (sum_weights[(width + 1) :] - sum_weights[: (-width - 1)])
+        )[::-1]
+
+    if method == "centered":
+        mean /= 2.0
+
+    return mean
