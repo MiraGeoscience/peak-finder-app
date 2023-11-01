4fb8f0c57b69f9649c90725b8d9666c64472b29b
dominiquef <dominiquef@mirageoscience.com>
2023-11-01 15:43:33 -0700

    Black fix

--- peak_finder/application.py
+++ peak_finder/application.py
@@ -24,7 +24,7 @@ from flask import Flask
 from geoapps_utils.application.application import get_output_workspace
 from geoapps_utils.application.dash_application import (
     BaseDashApplication,
-    ObjectSelection
+    ObjectSelection,
 )
 from geoapps_utils.plotting import format_axis, symlog
 from geoh5py import Workspace
