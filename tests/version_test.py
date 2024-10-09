c14f43909ad6d31f1b09c67910341b54cb922123
SophieCurinier <sophiec@mirageoscience.com>
2024-10-09 14:08:33 -0400

    [DEVOPS-504] Cleaning for PR

--- tests/version_test.py
+++ tests/version_test.py
@@ -7,12 +7,37 @@
 from __future__ import annotations
 
 import re
+from pathlib import Path
+
+import tomli as toml
+import yaml
+from jinja2 import Template
+from packaging.version import Version
 
 import peak_finder
 
 
+def get_conda_recipe_version():
+    path = Path(__file__).resolve().parents[1] / "meta.yaml"
+
+    with open(str(path), encoding="utf-8") as file:
+        content = file.read()
+
+    template = Template(content)
+    rendered_yaml = template.render()
+
+    recipe = yaml.safe_load(rendered_yaml)
+
+    return recipe["package"]["version"]
+
+
 def test_version_is_consistent(pyproject: dict):
     assert peak_finder.__version__ == pyproject["tool"]["poetry"]["version"]
+
+
+def test_conda_version_is_pep440():
+    version = Version(get_conda_recipe_version())
+    assert version is not None
 
 
 def test_version_is_semver():
