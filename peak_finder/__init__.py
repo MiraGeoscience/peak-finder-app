d9a1484328b80cc772aae4293a5070185d51407e
Sébastien Hensgen <sebastienh@mirageoscience.com>
2025-04-13 23:05:12 -0400

    [GEOPY-2049] bump version to alpha2

--- peak_finder/__init__.py
+++ peak_finder/__init__.py
@@ -7,7 +7,7 @@
 #  (see LICENSE file at the root of this source code package).                      '
 # '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-__version__ = "0.2.1-alpha.1"
+__version__ = "0.2.1-alpha.2"
 
 import os
 import warnings
